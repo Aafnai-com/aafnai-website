70eb5d13ac09505158ccf9dfbb8aa7ce61c2ffd9
MrSujiit <sujitshrestha0004@gmail.com>
2025-12-12 15:33:15 +0000

    Add - Popover demo card with data on nav links

--- frontend/src/components/ui/navbarDropdown.jsx
+++ frontend/src/components/ui/navbarDropdown.jsx
@@ -1,15 +1,41 @@
 import { Popover, Text } from "@mantine/core";
 
-export default function NavbarDropdown({ children, linkName}) {
-  return (
-    <Popover width={200}  offset={30} withArrow withinPortal arrowPosition="side" arrowOffset={5} arrowSize={5} arrowRadius={10}>
-      <Popover.Target>
-        {/* Make sure children is a single DOM element */}
-        {children}
-      </Popover.Target>
-      <Popover.Dropdown>
-        <Text size="sm">Dropdown content here {linkName}</Text>
-      </Popover.Dropdown>
-    </Popover>
-  );
+export default function NavbarDropdown({ children, linkName }) {
+
+    const dropdownContentMap = {
+        aafnaifor: (
+            <div>
+                <Text size="sm">Aafnai for</Text>
+                <ul>
+                    <li>Marketing</li>
+                    <li>SEO</li>
+                    <li>Ads Management</li>
+                </ul>
+            </div>
+        ),
+        services: (
+            <div>
+                <Text size="sm">Our Services:</Text>
+                <ul>
+                    <li>Marketing</li>
+                    <li>SEO</li>
+                    <li>Ads Management</li>
+                </ul>
+            </div>
+        ),
+    }
+
+    const contentToShow = dropdownContentMap[linkName.toLowerCase().replace(/\s+/g, "")] || (
+        <Text size="sm">No content available</Text>
+    );
+    return (
+        <Popover width={200} offset={30} withArrow withinPortal arrowPosition="side" arrowOffset={5} arrowSize={5} arrowRadius={10}>
+            <Popover.Target>
+                {children}
+            </Popover.Target>
+            <Popover.Dropdown>
+                {contentToShow} {linkName}
+            </Popover.Dropdown>
+        </Popover>
+    );
 }
