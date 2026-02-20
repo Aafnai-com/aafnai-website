0f8a30455787096c1f2d7a21fce40f4384c97eee
MrSujiit <sujitshrestha0004@gmail.com>
2026-02-20 05:12:21 +0000

    GIF issue

--- frontend/src/components/navbar.jsx
+++ frontend/src/components/navbar.jsx
@@ -33,7 +33,7 @@ export default function Navbar() {
 
                         <Grid.Col span={2} className="navbar-logo">
                             <a href="/" >
-                                <img src="../../src/assets/aafnai_black_background.gif" alt="Aafnai Ads Logo" />
+                                <img src="../../public/assets/png/aafnai_black_background.gif" alt="Aafnai Ads Logo" />
                             </a>
                         </Grid.Col>
 
